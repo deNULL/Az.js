6efb227e30366dd69daf9c93b9a910fc9a8fbc35
Denis Olshin <me@denull.ru>
2016-06-11 03:58:28 +0300

    wrote a bunch of JSDocs for Tokens and Morph

--- src/az.tokens.js
+++ src/az.tokens.js
@@ -25,6 +25,18 @@
   }
 
   /**
+   * Токен, соответствующий некоторой подстроке в представленном на вход тексте.
+   * @typedef {Object} Token
+   * @property {string} type Тип токена.
+   * @property {string} subType Подтип токена.
+   * @property {number} st Индекс первого символа, входящего в токен.
+   * @property {number} en Индекс последнего символа, входящего в токен.
+   * @property {number} length Длина токена.
+   * @property {boolean} firstUpper True, если первый символ токена является заглавной буквой.
+   * @property {boolean} allUpper True, если все символы в токене являются заглавными буквами.
+   */
+
+  /**
    * Создает токенизатор текста с заданными опциями.
    *
    * @playground
