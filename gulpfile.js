74edf9502cf7b5a77d0f28b91621d05073de1f28
Denis Olshin <me@denull.ru>
2016-06-11 21:20:08 +0300

    updated docs

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,29 @@ var gulp = require('gulp'),
     filesize = require('gulp-filesize'),
     sourcemaps = require('gulp-sourcemaps'),
     jsdoc2md = require('gulp-jsdoc-to-markdown'),
+    merge = require('merge-stream'),
     fs = require('fs');
 
 gulp.task('docs', function () {
-  return gulp.src(['src/az.tokens.js', 'src/az.morph.js'])
-    .pipe(jsdoc2md({ template: fs.readFileSync('./api.hbs', 'utf8') }))
-    .on('error', function (err) {
-      gutil.log(gutil.colors.red('jsdoc2md failed'), err.message)
-    })
-    .pipe(rename(function (path) {
-      path.basename = path.basename.split('.').map(function(part) {
-        return part[0].toLocaleUpperCase() + part.substr(1);
-      }).join('.');
-      path.extname = '.md'
-    }))
-    .pipe(gulp.dest('wiki'));
+  return merge(
+    gulp.src(['src/az.tokens.js', 'src/az.morph.js'])
+      .pipe(jsdoc2md({ template: fs.readFileSync('./api.hbs', 'utf8') }))
+      .on('error', function (err) {
+        gutil.log(gutil.colors.red('jsdoc2md failed'), err.message)
+      })
+      .pipe(rename(function (path) {
+        path.basename = path.basename.split('.').map(function(part) {
+          return part[0].toLocaleUpperCase() + part.substr(1);
+        }).join('.');
+        path.extname = '.md'
+      }))
+      .pipe(gulp.dest('wiki')),
+    gulp.src('README.md')
+      .pipe(rename(function (path) {
+        path.basename = 'Home';
+      }))
+      .pipe(gulp.dest('wiki'))
+  );
 });
 
 gulp.task('default', function() {
