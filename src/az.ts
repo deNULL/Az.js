22a9148bf8acfbd17c74a5b270b12773cd44111d
castaval <alexander.butusov.03@mail.ru>
2022-11-11 19:53:45 +0300

    ref: az.ts

--- src/az.ts
+++ src/az.ts
@@ -1,52 +1,41 @@
 /** @namespace Az **/
-let fs: any;
-if (typeof require != 'undefined' && typeof exports === 'object' && typeof module !== 'undefined') {
-  fs = require('fs');
+import * as fs from 'fs';
+
+interface Az {
+  load: (url: string, responseType: string, callback: (err: Error | null, data?: Buffer | ArrayBufferLike) => void) => void
+  extend: (_1: any, _2: any) => any
 }
 
-export let Az = {
-  load: function(url: string, responseType: string, callback: any) {
-    if (fs) {
-      fs.readFile(url, { encoding: responseType == 'json' ? 'utf8' : null }, function (err: any, data: any) {
-        if (err) {
-          callback(err);
-          return;
-        }
-
-        if (responseType == 'json') {
-          callback(null, JSON.parse(data));
-        } else
-        if (responseType == 'arraybuffer') {
-          if (data.buffer) {
-            callback(null, data.buffer);
-          } else {
-            let ab = new ArrayBuffer(data.length);
-            let view = new Uint8Array(ab);
-            for (let i = 0; i < data.length; ++i) {
-                view[i] = data[i];
-            }
-            callback(null, ab);
-          }
-        } else {
-          callback(new Error('Unknown responseType'));
-        }
-      });
-      return;
-    }
-
-    let xhr: any = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = responseType;
-
-    xhr.onload = function (e: any) {
-      if (xhr.response) {
-        callback && callback(null, xhr.response);
+export let Az : Az = {
+  load: function(url: string, responseType: string | Buffer, callback: (err: Error | null, data?: Buffer | ArrayBufferLike) => void): void {
+    fs.readFile(url, { encoding: responseType == 'json' ? 'utf8' : null }, function (err, data) {
+      if (err) {
+        callback(err);
+        return;
       }
-    };
 
-    xhr.send(null);
+      if (responseType == 'json') {
+        callback(null, JSON.parse(data as string));
+      } else if (responseType == 'arraybuffer') {
+        data = data as Buffer;
+
+        if (data.buffer) {
+          callback(null, data.buffer);
+        } else {
+          let ab = new ArrayBuffer(data.length);
+          let view = new Uint8Array(ab);
+          for (let i = 0; i < data.length; ++i) {
+              view[i] = data[i] as number;
+          }
+          callback(null, ab);
+        }
+      } else {
+        callback(new Error('Unknown responseType'));
+      }
+    });
+    return;
   },
-  extend: function(_1: any, _2: any) {
+  extend: function(_1: any, _2: any): any {
     let result: any = {};
     for (let i = 0; i < arguments.length; i++) {
       for (let key in arguments[i]) {
